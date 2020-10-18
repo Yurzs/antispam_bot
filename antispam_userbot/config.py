5d56c4a4967201289a727de5283819a8fc84216e
Yurzs <yurzs@icloud.com>
2020-10-18 20:48:52 +0300

    docker: fix session_path env

--- antispam_userbot/config.py
+++ antispam_userbot/config.py
@@ -6,7 +6,6 @@ SECRET = os.environ["SECRET"]
 API_ID = os.environ["API_ID"]
 SESSION_PATH = os.environ["SESSION_PATH"]
 
-print(SESSION_PATH)
 MONGO_DATABASE_URI = os.environ["MONGO_DATABASE_URI"]
 MONGO_DATABASE_NAME = os.environ["MONGO_DATABASE_NAME"]
 
